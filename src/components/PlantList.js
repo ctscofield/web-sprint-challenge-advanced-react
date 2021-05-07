fc68746bf31db9c8d80a2dcd980d83e4842b7a5e
ctscofield <ctscofield@gmail.com>
2021-05-07 09:10:23 -0600

    set up and rendering the data to plants

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+    state = {
+      plants: []
+    };
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
@@ -13,6 +16,12 @@ export default class PlantList extends Component {
     axios.get('http://localhost:3333/plants')
       .then(res => {
         console.log(res);
+        this.setState({
+          plants: res.data,
+        });
+      })
+      .catch(err => {
+        console.log(err);
       });
   }
 
